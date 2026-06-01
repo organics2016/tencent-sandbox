023271c8129c1dd87c7d96440ea6f096b42d66d6
organics <organics2015@outlook.com>
2026-06-01 11:06:43 +0800

    update

--- Scripts/sandbox-setup.cmd
+++ Scripts/sandbox-setup.cmd
@@ -1,3 +1,3 @@
-powershell.exe -Command "Set-DnsClientServerAddress -InterfaceAlias '以太网' -ServerAddresses '192.168.0.1'"
+@REM powershell.exe -Command "Set-DnsClientServerAddress -InterfaceAlias '以太网' -ServerAddresses '192.168.0.1'"
 
 powershell.exe -executionpolicy bypass -file C:\Scripts\addzh-cn.ps1
